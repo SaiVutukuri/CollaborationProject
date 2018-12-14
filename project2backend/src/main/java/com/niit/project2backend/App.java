37ef7478d18c25bb83d58f7912d961756896ae2f
sai <git clone https://SaiVutukuri@bitbucket.org/SaiVutukuri/demo.git>
2018-12-14 12:03:00 +0530

    User and Job Module

---
+++ project2backend/src/main/java/com/niit/project2backend/App.java
@@ -0,0 +1,14 @@
+package com.niit.project2backend;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.configuration.DBConfiguration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context=new AnnotationConfigApplicationContext(DBConfiguration.class);
+    }
+}
